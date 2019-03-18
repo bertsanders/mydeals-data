56fa2a6357f9247122ac56a1912a3a61d425e33a
Bert Sanders <thebert99@gmail.com>
2019-03-18 10:35:46 -0500

    Add files via upload
    stub project

---
+++ src/main/java/com/walmart/hack/discount/DiscountHackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.hack.discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountHackServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiscountHackServiceApplication.class, args);
+  }
+
+}
