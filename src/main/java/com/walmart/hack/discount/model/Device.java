6f91d37170c2d48867fdf5af7bfde0c5bdc773d5
Arthi <arthi.chinna@walmart.com>
2019-03-18 16:02:49 -0500

    device controller

--- src/main/java/com/walmart/hack/discount/model/Device.java
+++ src/main/java/com/walmart/hack/discount/model/Device.java
@@ -1,9 +1,20 @@
 package com.walmart.hack.discount.model;
 
-import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Device {
+
+  @Id
   private String deviceId;
   private String version;
   private String model;
