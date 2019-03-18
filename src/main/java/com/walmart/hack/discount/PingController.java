4dadc68e01a07f855e3adfea657436be66341ea0
b0s01ar <paul mcb -18>
2019-03-18 10:49:24 -0500

    adding ping endpoint

---
+++ src/main/java/com/walmart/hack/discount/PingController.java
@@ -0,0 +1,15 @@
+package com.walmart.hack.discount;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PingController {
+  @GetMapping("/")
+  public String ping() {
+    return "It works!";
+  }
+
+}
